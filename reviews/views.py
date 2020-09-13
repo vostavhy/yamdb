af731f64e027a70b5a2884d5162edaa3f971f6d7
Vostavhy <vostavhy@gmail.com>
2020-09-13 17:50:10 +0300

    fix views

--- reviews/views.py
+++ reviews/views.py
@@ -32,11 +32,11 @@ class CommentViewSet(viewsets.ModelViewSet):
                           IsOwnerOrAdminOrModerOrReadOnly,)
 
     def perform_create(self, serializer):
-        title = get_object_or_404(Title, pk=self.kwargs.get('title_id'))
+        # title = get_object_or_404(Title, pk=self.kwargs.get('title_id'))
         review = get_object_or_404(Review, pk=self.kwargs.get('review_id'))
         serializer.save(author=self.request.user, review=review)
 
     def get_queryset(self):
-        title = get_object_or_404(Title, pk=self.kwargs.get('title_id'))
+        # title = get_object_or_404(Title, pk=self.kwargs.get('title_id'))
         review = get_object_or_404(Review, pk=self.kwargs.get('review_id'))
         return review.comments.all().order_by('id')
